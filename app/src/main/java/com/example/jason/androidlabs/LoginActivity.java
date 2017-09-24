5b2e7bd18cb2b21e8a244053206dfd06c0fa976a
nguy0476 <nguy0476@algonquinlive.com>
2017-09-24 11:07:48 -0400

    Updating Lab 3 files

--- app/src/main/java/com/example/jason/androidlabs/LoginActivity.java
+++ app/src/main/java/com/example/jason/androidlabs/LoginActivity.java
@@ -2,12 +2,63 @@ package com.example.jason.androidlabs;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+
+import android.widget.*;
+import android.view.*;
 
 public class LoginActivity extends Activity {
+
+    private Button loginButton;
+
+    protected static final String ACTIVITY_NAME = "LoginActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginButton = findViewById(R.id.Button_Login);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                sendLoginMessage(v);
+            }
+        });
     }
+
+    public void sendLoginMessage(View v){
+        Toast.makeText(LoginActivity.this, "Message", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(ACTIVITY_NAME, "In onStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(ACTIVITY_NAME, "In onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(ACTIVITY_NAME, "In onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(ACTIVITY_NAME, "In onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(ACTIVITY_NAME, "In onDestroy()");
+    }
+
 }
--- app/src/main/java/com/example/jason/androidlabs/StartActivity.java
+++ app/src/main/java/com/example/jason/androidlabs/StartActivity.java
@@ -2,12 +2,45 @@ package com.example.jason.androidlabs;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class StartActivity extends AppCompatActivity {
+
+    protected static final String ACTIVITY_NAME = "LoginActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i(ACTIVITY_NAME, "In onStart()");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(ACTIVITY_NAME, "In onResume()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(ACTIVITY_NAME, "In onPause()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(ACTIVITY_NAME, "In onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(ACTIVITY_NAME, "In onDestroy()");
     }
 }
